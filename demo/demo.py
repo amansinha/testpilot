7eb1059706dfb236c275fb05ae4ec5020487c787
Aman Sinha <amansinha@users.noreply.github.com>
2020-09-24 15:16:10 -0400

    remove unnecessary path stuff

--- demo/demo.py
+++ demo/demo.py
@@ -43,14 +43,6 @@ import os
 import sys
 import time
 
-try:
-    sys.path.append(glob.glob('../carla/CARLA/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
-        sys.version_info.major,
-        sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-except IndexError:
-    pass
-
 import carla
 sys.path.append('../')
 import testpilot
